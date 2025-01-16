3c94ec73818b5793ba32c36ff14bc06bdd519c34
geraltamirano22 <geraltamirano10@gmail.com>
2025-01-16 16:09:46 -0300

    Agrega mapper para licencia

---
+++ src/application/mappers/licencia.mapper.ts
@@ -0,0 +1,15 @@
+import { type PostLicenciaDto } from "../dtos/licencia/postLicencia.dto";
+import { Licencia } from "../../domain/entities/licencia.entity";
+//Importar conductor y su mapper
+
+export class LicenciaMapper{
+    public static fromPostDtoToEntity(postLicenciaDto: PostLicenciaDto): Licencia {
+        
+        const { categoria, fechaVenc, numero } = postLicenciaDto;
+
+        return new Licencia(categoria, new Date(fechaVenc), numero);
+    }
+}
+
+
+
