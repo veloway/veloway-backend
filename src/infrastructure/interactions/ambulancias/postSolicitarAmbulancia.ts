d13fdbdd05cc51bc4c56ab82ade1798f03659e37
JoaquinB3 <joaquinbotteri3@gmail.com>
2025-02-01 13:08:59 -0300

    feat: agrega conexion a la API ambulancia

---
+++ src/infrastructure/interactions/ambulancias/postSolicitarAmbulancia.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { type Checkpoint } from '../../../domain/entities/checkpoint.entity';
+import { type Conductor } from '../../../domain/entities/conductor.entity';
+import { obtenerDireccion } from '../../geocodingAPI/geocodingApi';
+
+const URL_AMBULANCIA = 'https://ambulanciaya.onrender.com/ambulancias/solicitar';
+
+export async function postSolicitarAmbulancia(conductor: Conductor, checkpoint: Checkpoint): Promise<void> {
+  const direccionRecuperada = await obtenerDireccion(checkpoint);
+
+  const solicitudAmbulancia = {
+    nombre: `${conductor.getNombre()} ${conductor.getApellido()}`,
+    telefono: `${conductor.getTelefono()}`,
+    direccion: `${direccionRecuperada}`,
+    description: 'Emergencia conductor Veloway'
+  };
+
+  await axios.post(URL_AMBULANCIA, solicitudAmbulancia);
+}
